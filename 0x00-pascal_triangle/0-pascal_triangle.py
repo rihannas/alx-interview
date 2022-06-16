c33768c8ac7954d4f20d63752ca0d4087ba20782
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-16 21:16:56 +0300

    update file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,14 +6,31 @@ representing the Pascals triangle of n:
 Returns an empty list if n <= 0
 You can assume n will be always an integer
 '''
+
+
 def pascal_triangle(n):
-    pascals_triangle = []
+    """
+    Return a list of lists, or a null list if n <=0
+    """
+    triangle = []
+    if n <= 0:
+        return []
+    for x in range(1, n+1):
+        if x == 1:
+            triangle.append([1])
+            continue
+        triangle.append(get_floor(triangle[x - 2]))
+    return triangle
 
-    for i in range(n):
-        pascals_triangle.append([1]*(i+1))
-  
-    for i in range(2,n):
-        for j in range(1,i):
-            pascals_triangle[i][j] = pascals_triangle[i-1][j-1] + pascals_triangle[i-1][j]
 
-    return pascals_triangle
+def get_floor(floor):
+    """
+    Creates the triangle's floor
+    """
+    floor_list = [1, 1]
+    size = len(floor) - 1
+
+    if size > 0:
+        for i in range(size):
+            floor_list.insert(i + 1, floor[i] + floor[i + 1])
+    return floor_list
