72083a999aa8a4dd07c7d6f859673741fb07579b
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-23 15:54:13 +0300

    task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
+
+Write a method that determines if all the boxes can be opened.
+
+Prototype: def canUnlockAll(boxes)
+boxes is a list of lists
+A key with the same number as a box opens that box
+You can assume all keys will be positive integers
+There can be keys that do not have boxes
+The first box boxes[0] is unlocked
+Return True if all boxes can be opened, else return False
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    a method that determines if all the boxed can be opened or no
+    """
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
