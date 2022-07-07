2bbc72dfaff54345e6e7df4ab81e8a2aa03946a2
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-08 00:48:23 +0300

    update file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
-"""Module to write a script of dict of Filesize and code_status"""
+"""Log Parser"""
 import sys
 
 
-filesize = 0
-count = 0
-stc = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-try:
-    for line in sys.stdin:
-        line = line.split()
+if __name__ == '__main__':
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
+
+    def parse_line(line):
+        """ Checks the line for matches """
         try:
-            filesize += int(line[-1])
-        except Exception:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
             pass
 
-        try:
-            code = line[-2]
-            if code in list(stc.keys()):
-                stc[code] += 1
-        except Exception:
-            pass
-        count += 1
-
-        if count % 10 == 0:
-            print("File size: {:d}".format(filesize))
-            for key in sorted(stc.keys()):
-                if stc[key] != 0:
-                    print("{:s}: {:d}".format(key, stc[key]))
-
-    print("File size: {:d}".format(filesize))
-    for key in sorted(stc.keys()):
-        if stc[key] != 0:
-            print("{}: {:d}".format(key, stc[key]))
-
-except KeyboardInterrupt:
-    print("File size: {:d}".format(filesize))
-    for key in sorted(stc.keys()):
-        if stc[key] != 0:
-            print("{:s}: {:d}".format(key, stc[key]))
-    raise
+    linenum = 1
+    try:
+        for line in sys.stdin:
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
